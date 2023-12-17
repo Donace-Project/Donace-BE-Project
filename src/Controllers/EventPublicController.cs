3cc20c56db4e4f754cd2889fd23b83e26d0c84ce
tieptv <tieptv>
2023-12-17 23:43:33 +0700

    update

---
+++ src/Controllers/EventPublicController.cs
@@ -0,0 +1,24 @@
+﻿using Donace_BE_Project.Interfaces.Services.Event;
+using Donace_BE_Project.Models.Event.Output;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Donace_BE_Project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventPublicController : ControllerBase
+    {
+        private readonly IEventService _service;
+        public EventPublicController(IEventService eventService)
+        {
+            _service = eventService;
+        }
+
+        [HttpGet("detail-by-id")]
+        public async Task<EventDetailModel> GetDetailById(Guid id)
+        {
+            return await _service.GetDetailByIdAsync(id);
+        }
+    }
+}
